1ab9e9c6024f30178df8ee76425c6bfa3fa2f043
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-09-23 10:49:31 +0200

    commit

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,7 +4,17 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
-
+function timerStatus(timeLeft){
+  if(timeLeft === 0){
+    return "Phil's cake is ready!"
+  }
+  else if(timeLeft === undefined){
+    return "You didn't set a timer!"
+  }
+  else{
+    return "The cake is still baking!"
+  }
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,7 +23,12 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
-
+function estimatePrepTime(ingredients, prepTime){
+  if(prepTime === undefined){
+    prepTime = 2
+  }
+  return ingredients.length * prepTime
+}
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
 // - a list of ingredients
@@ -29,7 +44,17 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
-
+function calculateQuantities(ingredients, layers){
+  let sugarAmount = 0
+  let eggAmount = 0
+  if(ingredients.includes('sugar')){
+    sugarAmount = 100 * layers
+  }
+  if(ingredients.includes('eggs')){
+    eggAmount = 2 * layers
+  }
+  return {sugar: sugarAmount, eggs: eggAmount}
+}
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,7 +67,13 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+function improveRecipe(quantities, portions){
+  let newQuantities = {}
+  for(let i in quantities){
+    newQuantities[i] = quantities[i] * portions
+  }
+  return newQuantities
+}
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
